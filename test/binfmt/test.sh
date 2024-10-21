316faa66fb6ec4a13618eb3d79a09a2ce88a00dc
Guoxin Pu <pugokushin@gmail.com>
2024-10-21 11:05:19 +0800

    test/binfmt: remove hello file after testing

--- test/binfmt/test.sh
+++ test/binfmt/test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 echo -n "all:" > Makefile
 printf ' test-%s' x86_64 aarch64 riscv64 loongarch64  >> Makefile
 echo >> Makefile
@@ -9,13 +9,14 @@ for arch in \
 	loongarch64 \
 ;
 do
-	#arch=${triplet%%-*}
 	triplet="${arch}-linux-gnu"
-	printf 'hello-%s: hello.c\n\t%s-gcc -DARCH=\\"%s\\" -static -o $@ $^\n' "$arch" "$triplet" "$arch"
+	printf 'hello-%s: hello.c\n\t%s-gcc -DARCH=\\"%s\\" -o $@ $^\n' "$arch" "$triplet" "$arch"
 	if  [[ "$arch" == $(uname -m) ]]; then
 		printf 'test-%s: hello-%s\n\t[[ "$(shell ./hello-%s)" == "Hello from architecture: %s" ]]\n' "$arch" "$arch" "$arch" "$arch"
 	else
 		printf 'test-%s: hello-%s\n\t[[ "$(shell /usr/%s/lib/ld-linux-*.so.* --library-path /usr/%s/lib ./hello-%s)" == "Hello from architecture: %s" ]]\n' "$arch" "$arch" "$triplet" "$triplet" "$arch" "$arch"
 	fi
+	printf '\trm -f hello-%s\n' "$arch"
 done >> Makefile
 make
+rm -f Makefile hello-*
